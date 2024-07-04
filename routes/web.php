a1b2a86a6e5c152d474cae61e227175af56e1575
DevChirayu <chirayupatil23@gmail.com>
2024-07-04 21:34:20 +0530

    Added 5 pages
    * one page added
    * second page added
    * third page added
    * forth page remote added
    * fifth page remote added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/one', function () {
+    return "Welcome to First Page";
+});
+
+Route::get('/two', function () {
+    return "Welcome to Second Page";
+});
+
+Route::get('/three', function () {
+    return "Welcome to Third Page";
+});
+
+Route::get('/four', function () {
+    return "Welcome to Forth Page";
+});
+
+Route::get('/five', function () {
+    return "Welcome to Fifth Page";
+});
